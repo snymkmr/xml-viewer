cfe5d0a136710984fc8c974b148e26f23f39948f
Sanyam Kumar <sanyam.kumar@outlook.in>
2024-01-26 00:58:32 +0530

    XML viewer initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    });
+    
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+ipcMain.on('file-selected', (event, filePath) => {
+    console.log(`File selected: ${filePath}`);
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file: ${err.message}`);
+            event.sender.send('xml-data', `Error reading file: ${err.message}`);
+        } else {
+            event.sender.send('xml-data', data);
+        }
+    });
+});
